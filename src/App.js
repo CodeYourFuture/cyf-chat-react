e6bad9e353836ffee8ca39e0bfaed850a15fface
Alex <alex.andrul10@gmail.com>
2020-07-01 20:32:39 +0100

    fetch messages

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [messages, setMessages] = useState([]);
+  useEffect(() => {
+    fetch("https://cyf-chat-server-express.herokuapp.com/messages")
+      .then((res) => res.json())
+      .then((data) => setMessages(data));
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="container">
+          {messages.map((mess) => (
+            <div className="message">
+              <h3>{mess.from}</h3>
+              <p>{mess.text}</p>
+            </div>
+          ))}
+        </div>
       </header>
+      <form>
+        <input type="text"></input>
+        <input type="text"></input>
+      </form>
     </div>
   );
 }
