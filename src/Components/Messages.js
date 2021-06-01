045b28eec2eda3e55a012bd2f9ad1c65ef1216d0
ali <alinosratipour@gmail.com>
2021-06-01 15:51:18 +0100

    create post function and added css to the page

--- src/Components/Messages.js
+++ src/Components/Messages.js
@@ -1,42 +1,56 @@
 import React ,{ useEffect,useState } from 'react';
 
 
-
-
-const Messages = () =>{
-
-
-  const [messages, setMessages] = useState([]);
-  const [newMessage, setNewMessages] = useState({
-    from: "",
-    text: "",
+  const Messages = () =>{
+    const [messages, setMessages] = useState([]);
+    const [newMessage, setNewMessages] = useState({
+      from: "",
+      text: "",
   });
 
+  // Post New Messages
   function postMessage(url, data) {
-    const response = fetch(url, {
+     const response = fetch(url, {
       method: "POST",
       mode: "cors",
       headers: {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(data),
-    }).then(() => console.log("Message Send!"));
+    });
     return response;
   }
+
+  // Submit the form
   const handleSubmit = (e) => {
     e.preventDefault();
-
-    console.log(newMessage);
     postMessage(
       "https://ali-nosratipour-chat-server.glitch.me/messages",
       newMessage
     );
   };
 
-  const handleOnChange = (e) => {
-    const some = { ...newMessage, [e.target.name]: e.target.value };
-    setNewMessages(some);
+  const handleMessage = (e) => {
+    const messageContent= { ...newMessage, [e.target.name]: e.target.value };
+    console.log(messageContent);
+    setNewMessages(messageContent);
   };
+
+ 
+  // const handleDelete = (deleteId) => {
+
+  //   //const deleteId = e.target.value;
+  //   fetch(
+  //     `https://ali-nosratipour-chat-server.glitch.me/messages/${deleteId}`,{
+      
+  //      method:'DELETE',
+  //      headers:{'Accept':'Application/json',
+  //       'Content-Type': 'Application/json'
+  //      }
+  //     });
+     
+   
+  // };
 
 
   useEffect(() => {
@@ -44,92 +58,52 @@ const Messages = () =>{
       .then((res) => res.json())
       .then((data) => setMessages(data));
   }, [messages]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="wrapper">
         <div className="container">
           {messages.map((mess) => (
             <div key={mess.id}>
+                <hr></hr>
               <h5>{mess.from}</h5>
+            
               <div className="message">
                 <p>{mess.text}</p>
               </div>
-              <span>{mess.timeSent}</span>
+             
+
             </div>
           ))}
         </div>
-      </header>
-      <form onSubmit={handleSubmit}>
+      <div className="form-Container">
+      <form onSubmit={handleSubmit} className="form-input">
         <input
+          className="userName"
           type="text"
           placeholder="Your Name"
-          onChange={handleOnChange}
+          onChange={handleMessage}
           name="from"
         ></input>
         <textarea
+          className="message"
           type="text"
           placeholder="Enter message here..."
           rows="6"
           cols="50"
-          onChange={handleOnChange}
+          onChange={handleMessage}
           name="text"
         ></textarea>
         <button>Send</button>
       </form>
+      </div>
     </div>
   );
 
 
 
 
-    useEffect(() =>{
-        getMessage();
-      },[])
-      
     
-
-
-const getMessage = () =>{
-  fetch(`https://ali-nosratipour-chat-server.glitch.me/messages`)
-  .then(res => res.json())
-  .then(json => setMessages(json))
-}
-
-
-
-
-      const handelLatest = () =>{
-        getMessage();
-      }
-      
-    
-
-      return (
-        <div className="App">
-         
-        <div className="btn-holder">
-          <button    onClick={handelLatest} >See Latest</button>
-
-       </div>  
-        <div className="wrapper">
-         <div className="container">
-       <div>{messages.map(mes =>{
-         return(
-           <div>
-             <h3>{mes.from}</h3>
-              <p>{mes.text}</p>
-           </div>
-         );
-       })}</div>
-      
-       </div>
-        </div>
-        
-        </div>
-      );   
-
-
-
 }
 
 export default Messages;
