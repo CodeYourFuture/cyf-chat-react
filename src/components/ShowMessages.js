621185ed393627edddd7a02c7faa36286e97027d
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-11-12 14:53:47 +0000

    feat: delete button add

--- src/components/ShowMessages.js
+++ src/components/ShowMessages.js
@@ -1,11 +1,20 @@
 import React from "react";
 
-export default function ShowMessages({ messages }) {
+
+export default function ShowMessages({ messages, fetchData }) {
+  function handleDelete (id) {
+      fetch(`https://gulnihal-node-challange-chat-server.glitch.me/messages/${id}`, {
+        method: "DELETE",
+        
+      }).then((response) => fetchData());
+  }
   const messagesArr = messages.map((message) => {
     return (
       <div key={message.id}>
         <h2>{message.from}</h2>
         <p>{message.text}</p>
+        <button onClick={()=>handleDelete(message.id)}> Delete </button> 
+      {/* callback function prevents evoking onClick */}
       </div>
     );
   });
