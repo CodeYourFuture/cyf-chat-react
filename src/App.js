7718dcb9a53ed457110f10109ce19c57b434e8e3
Gulnihal Naldoken <gh.naldoken@gmail.com>
2021-11-12 12:39:45 +0000

    feat: show all messages button

--- src/App.js
+++ src/App.js
@@ -1,2 +1,16 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ShowMessages from "./components/ShowMessages";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello world</h1>
+      a
+      <button onClick={getAllMessages}>Show all messages</button>
+      <ShowMessages messages={messages} />
+    </div>
+  );
+}
 
 export default App;
