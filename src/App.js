9b3f035b569ccfb21d96e1397a3166f020bffbe0
Amanda Obiajulu Nwadukwe <xtremeli.basic@gmail.com>
2022-01-12 19:00:12 +0000

    chat bot started
    Chat bot working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Message from './Message';
 
 function App() {
+  const [message, setMessage] = useState("");
+  const [messagesArray, setMessagesArray] = useState([]);
+  const [sender, setSender] = useState("");
+  const [submittedObject, setSubmittedObject] = useState({});
+
+useEffect(() => {
+  console.log(submittedObject)
+    const requestOptions = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(submittedObject)
+        
+    };
+    fetch('http://localhost:5000/create', requestOptions)
+    .then(response => {
+      response.json()})
+    .then(data => {
+      console.log(data)});
+      
+}, [submittedObject]);
+
+  function toSetName(event){
+    setSender(event.target.value);
+  }
+
+  function toSetMessage(event){
+    setMessage(event.target.value);
+  }
+
+  //This function sends the user message
+  function sendMessage(event){
+    event.preventDefault();
+    setSubmittedObject({
+      "message" : message,
+      "sender" : sender
+    });
+
+    setMessagesArray(messagesArray.concat(submittedObject));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="messageContainer">
+          <Message />
+          <div className="controls">
+              <input type="text" placeholder="please type in your name" onChange={toSetName} value={sender}/>
+            <input type="text" placeholder='type a message to send' onChange={toSetMessage} value={message}/>
+            <button type="submit" onClick={sendMessage}>Send</button>
+            <button type="submit" onClick={sendMessage}>See Latest</button>
+          </div>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
