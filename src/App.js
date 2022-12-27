98db629196984d7273150950a8892b3535e9bda6
SaadiaELF <saadiaelfekak@gmail.com>
2022-12-27 19:14:10 +0000

    Added card component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
 import React from "react";
-import { ChakraProvider } from "@chakra-ui/react";
+import {
+  ChakraProvider,
+  Card,
+  CardHeader,
+  CardBody,
+  CardFooter,
+  Heading,
+  Image,
+  Center,
+} from "@chakra-ui/react";
 import "./App.css";
 
 function App() {
-  return <ChakraProvider></ChakraProvider>;
+  return (
+    <ChakraProvider>
+      <Center>
+        <Card
+          m="4"
+          width="lg"
+          direction={{ base: "column", sm: "row" }}
+          overflow="hidden"
+          variant="outline"
+        >
+          <Image
+            objectFit="cover"
+            maxW={{ base: "100%", sm: "200px" }}
+            src="https://images.pexels.com/photos/1111369/pexels-photo-1111369.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+            alt="Pink Background With Speech Bubble"
+          />
+          <CardBody>
+            <CardHeader>
+              <Heading size="md">CYF Chat</Heading>
+            </CardHeader>
+          </CardBody>
+        </Card>
+      </Center>
+    </ChakraProvider>
+  );
 }
 
 export default App;
