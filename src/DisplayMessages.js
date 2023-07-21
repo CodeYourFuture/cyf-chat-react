93300547ec99a2f52a92c12cf2bcb2d61e06fcd7
Delnia Alipour <delniaalipur@gmail.com>
2023-07-21 22:16:08 +0100

    update Display messages

--- src/DisplayMessages.js
+++ src/DisplayMessages.js
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from "react";
+import DeleteMessageButton from "./DeleteMessageButton";
+import CreateMessageForm from "./CreateMessageForm";
 
 function DisplayMessages() {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    fetch("https://delnia-alipour-chat-server.glitch.me/messages/latest")
+    fetch("https://delnia-alipour-chat-server.glitch.me/messages")
       .then((res) => res.json())
       .then((data) => setMessages(data));
   }, []);
 
+  const handleDeleteMessage = (id) => {
+    setMessages((prevMessages) =>
+      prevMessages.filter((message) => message.id !== id)
+    );
+  };
+
+  const handleCreateMessage = (newMessage) => {
+    setMessages((prevMessages) => [...prevMessages, newMessage]);
+  };
   return (
     <div>
       <h2>Latest messages</h2>
@@ -17,10 +28,15 @@ function DisplayMessages() {
           return (
             <li key={message.id}>
               {message.from}: {message.text}
+              <DeleteMessageButton
+                messageId={message.id}
+                onDelete={handleDeleteMessage}
+              />
             </li>
           );
         })}
       </ul>
+      <CreateMessageForm onMessageCreated={handleCreateMessage} />
     </div>
   );
 }
