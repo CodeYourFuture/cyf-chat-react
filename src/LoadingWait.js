231200329a10e331e54ab4364a70288448e6f3a4
malkitbenning <malkitbenning@gmail.com>
2023-07-28 13:27:28 +0100

    implemented loading...

---
+++ src/LoadingWait.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./LoadingWait.css";
+
+function LoadingWait() {
+  return (
+    <div className="loading-container">
+      <p className="loading-text">Loading...Please wait...</p>
+    </div>
+  );
+}
+
+export default LoadingWait;
--- src/Messages.js
+++ src/Messages.js
@@ -1,40 +1,26 @@
 import React, { useState, useEffect } from "react";
-import "./Messages.css";
-import Message from "./Message";
 import MessageForm from "./MessageForm";
+import MessageList from "./MessageList";
+import LoadingWait from "./LoadingWait";
 
 function Messages() {
   const [chats, setChats] = useState([]);
-  const [deleteRow, setDeleteRow] = useState(null);
+  const [chatsReady, setChatsReady] = useState(false);
+
   useEffect(() => {
+    setChatsReady(false);
     fetch("https://malkit-chat-server.glitch.me/messages")
       .then((res) => res.json())
       .then((data) => {
         setChats(data);
+        setChatsReady(true);
       });
   }, []);
-
-  useEffect(() => {
-    if (deleteRow || deleteRow === 0)
-      fetch("https://malkit-chat-server.glitch.me/messages/" + deleteRow, { method: "DELETE" })
-        .then((res) => res.json())
-        .then((data) => {
-          setChats(data);
-        });
-  }, [deleteRow]);
 
   return (
     <div>
       <MessageForm setChats={setChats} />
-      <ul className="message-list">
-        {chats.map((chat) => {
-          return (
-            <li key={chat.id}>
-              <Message chat={chat} deleteRow={deleteRow} setDeleteRow={setDeleteRow} />
-            </li>
-          );
-        })}
-      </ul>
+      {chatsReady ? <MessageList chats={chats} setChats={setChats} /> : <LoadingWait />}
     </div>
   );
 }
